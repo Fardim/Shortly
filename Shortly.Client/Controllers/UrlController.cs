beb6c593eb06735253bb2241c777aea7fc843c9e
fardim <fardimkaiser@gmail.com>
2024-02-01 20:17:19 +0600

    Home page design ready

---
+++ Shortly.Client/Controllers/UrlController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shortly.Client.Data.Models;
+
+namespace Shortly.Client.Controllers
+{
+    public class UrlController : Controller
+    {
+        public IActionResult Index()
+        {
+            var url = new Url
+            {
+                Id = 1,
+                OriginalLink = "http://google.com",
+                ShortLink = "shrtly",
+                NrOfClicks = 1,
+                UserId = 1,
+            };
+            var allData = new List<Url>();
+            allData.Add(url);
+            return View(allData);
+        }
+    }
+}
